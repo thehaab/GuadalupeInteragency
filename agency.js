686960b3feb8e2373388269c7713744b0acf08e8
Travis Church <thehaab@gmail.com>
2024-03-19 19:14:44 -0500

    Add files via upload

---
+++ agency.js
@@ -0,0 +1,38 @@
+const agencies = [
+  {
+    name: "Agency 1",
+    contactName: "John Doe",
+    contactTitle: "Director",
+    contactPhone: "123-456-7890",
+    contactEmail: "info@agency1.com",
+    altContactName: "Jane Doe",
+    altContactTitle: "Assistant Director",
+    altContactPhone: "123-456-7891",
+    altContactEmail: "",
+    website: "www.agency1.com",
+
+    hours: "9am-5pm",
+    location: "City 1",
+    mailingAddress: "123 Main St, City 1, State 1, 12345",
+    fees: "$50",
+    waitTime: "2 weeks",
+    qualigfications: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    countiesServed: ["County 1", "County 2"],
+    additionalInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    name: "Agency 2",
+    contactName: "Jane Smith",
+    contactPhone: "987-654-3210",
+    website: "www.agency2.com",
+    email: "info@agency2.com",
+    hours: "8am-6pm",
+    location: "City 2",
+    fees: "$100",
+    waitTime: "1 month",
+    countiesServed: ["County 3", "County 4"],
+    additionalInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  // Add more agency objects as needed
+];
+export default agencies;
